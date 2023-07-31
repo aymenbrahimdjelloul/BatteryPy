98d8ff42a27b3fe8874c3649fea1af6a1f82a6ba
Aymen Brahim Djelloul <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2023-07-31 15:11:40 +0100

    Add files via upload

---
+++ BatteryPy/exceptions.py
@@ -0,0 +1,37 @@
+"""
+@author: Aymen Brahim Djelloul
+version: 0.0.1
+License: MIT
+
+"""
+
+# IMPORTS
+import sys
+
+
+class NotSupportedPlatform(BaseException):
+
+    def __init__(self, current_platform: str):
+        self.current_platform = current_platform
+
+    def __str__(self) -> str:
+        return f"BatteryPy is doesn't support '{self.current_platform}' Operating system."
+
+
+class NotSupportedDriver(BaseException):
+
+    def __init__(self, driver: str):
+        self.driver_name = driver
+
+    def __str__(self) -> str:
+        return f"BatteryPy can't reach '{self.driver_name}' driver which is necessary\nTry to update your system."
+
+
+class NotSupportedDeviceType(BaseException):
+
+    def __str__(self):
+        return f"Your Device Doesn't have Battery or it can't be reached"
+
+
+if __name__ == "__main__":
+    sys.exit()
