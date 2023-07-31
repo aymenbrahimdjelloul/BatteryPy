75574e8c307c1a73cfcd94170e32383507d5bfbb
Aymen Brahim Djelloul <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2023-07-31 21:45:08 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
-from BatteryPy import Battery
+"""
+@author : Aymen Brahim Djelloul.
+This file is for testing BatteryPy which we will get the battery informations
+"""
 
+# Import the Battery Object class from BatteryPy module
+from BatteryPy.BattryPy import Battery
 
+# Create the Battery object 
 battery = Battery()
 
-print(battery.manufacturer)
-print(battery.type)
-print(battery.chemistry)
-print(battery.battery_percentage)
-print(battery.battery_health)
-print(battery.is_fast_charging)
-print(battery.is_plugged)
-print(battery.get_current_voltage())
-print(battery.power_management_mode)
+# Print out all battery informations
+print(battery.manufacturer)              # Get the Battery manufacturer name
+print(battery.type)                      # Get the battery type (Interna Battery or external)
+print(battery.chemistry)                 # Get the Battery chemistry technology (Lithuim Ion in most cases)
+print(battery.battery_percentage)        # Get the battery percentage (charging level)
+print(battery.battery_health)            # Get the battery health or battery maximum capacity in percentage
+print(battery.is_fast_charging)          # tells if the battery is fast charging or not
+print(battery.is_plugged)                # tells if the battery is plugged to the power source
+print(battery.get_current_voltage())     # Get the current battery output voltage
+print(battery.power_management_mode)     # Get the power maangement mode is it 'balanced' or 'performance' or 'economy'
