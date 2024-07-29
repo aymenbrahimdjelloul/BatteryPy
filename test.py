4da3d0196b80b506508216420d4797c244b84d86
Mr Meow <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2024-07-29 13:13:12 +0100

    Add files via upload
    Update the testing file to a proper CLI ( Command Line Interface )

--- test.py
+++ test.py
@@ -1,21 +1,53 @@
 """
 @author : Aymen Brahim Djelloul.
-This file is for testing BatteryPy which we will get the battery informations
+This file is for testing BatteryPy which we will get the battery information
 """
 
 # Import the Battery Object class from BatteryPy module
-from BatteryPy.BattryPy import Battery
+from BatteryPy.battery_py import Battery, AUTHOR, VERSION
+import os
 
-# Create the Battery object 
-battery = Battery()
 
-# Print out all battery informations
-print(battery.manufacturer)              # Get the Battery manufacturer name
-print(battery.type)                      # Get the battery type (Interna Battery or external)
-print(battery.chemistry)                 # Get the Battery chemistry technology (Lithuim Ion in most cases)
-print(battery.battery_percentage)        # Get the battery percentage (charging level)
-print(battery.battery_health)            # Get the battery health or battery maximum capacity in percentage
-print(battery.is_fast_charging)          # tells if the battery is fast charging or not
-print(battery.is_plugged)                # tells if the battery is plugged to the power source
-print(battery.get_current_voltage())     # Get the current battery output voltage
-print(battery.power_management_mode)     # Get the power maangement mode is it 'balanced' or 'performance' or 'economy'
+def main():
+    """ This function will perform the test of BatteryPy"""
+
+    # Create the Battery object
+    battery = Battery()
+
+    # Set terminal window title
+    os.system(f"title BatteryPy - {VERSION}")
+    # Print the software header
+    print(f"\n{' '*10}BatteryPy - v{VERSION}{' '*15} Developed by {AUTHOR}\n\n\n")
+
+    # Print out all battery information
+    print(f"Battery manufacturer : {battery.manufacturer}\n")
+    # Get the Battery manufacturer namedd
+
+    print(f"Battery Type : {battery.type}\n")
+    # Get the battery type (Internal Battery or external)
+
+    print(f"battery Chemistry : {battery.chemistry}\n")
+    # Get the Battery chemistry technology (Lithium-Ion in most cases)
+
+    print(f"Battery Charge : {battery.battery_percentage}\n")
+    # Get the battery percentage (charging level)
+
+    print(f"Battery Health : {battery.battery_health}\n")
+    # Get the battery health or battery maximum capacity in percentage
+
+    print(f"Is Fast Charge ? : {'YES' if battery.is_fast_charging else 'NO'}\n")
+    # tells if the battery is fast charging or not
+
+    print(f"Is Charging ? : {'YES' if battery.is_plugged else 'NO'}\n")
+    # tells if the battery is plugged to the power source
+
+    print(f"Battery Voltage : {battery.get_current_voltage()}\n")
+    # Get the current battery output voltage
+
+    print(f"Power Mode : {battery.power_management_mode}\n")
+    # Get the power management mode is it 'balanced' or 'performance' or 'economy'
+    input("PRESS ENTER TO QUIT .")
+
+
+if __name__ == "__main__":
+    main()
