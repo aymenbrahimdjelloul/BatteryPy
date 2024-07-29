c574dc57ba102243d181c23215e165530c2ebd64
Mr Meow <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2024-07-29 23:52:14 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 """
 @author : Aymen Brahim Djelloul.
+date : 29.07.2024
+
 This file is for testing BatteryPy which we will get the battery information
 """
 
 # Import the Battery Object class from BatteryPy module
-from BatteryPy.battery_py import Battery, AUTHOR, VERSION
+from exceptions import *
+from batterypy import Battery, AUTHOR, VERSION
+from time import perf_counter
 import os
+
+# Set terminal window title
+os.system(f"title BatteryPy - {VERSION}")
+# Print the software header
+print(f"\n{' ' * 5}[ BatteryPy - v{VERSION}{' ' * 5}|{' '*5} Developed by {AUTHOR} ]\n\n")
 
 
 def main():
@@ -14,40 +23,54 @@ def main():
     # Create the Battery object
     battery = Battery()
 
-    # Set terminal window title
-    os.system(f"title BatteryPy - {VERSION}")
-    # Print the software header
-    print(f"\n{' '*10}BatteryPy - v{VERSION}{' '*15} Developed by {AUTHOR}\n\n\n")
-
     # Print out all battery information
-    print(f"Battery manufacturer : {battery.manufacturer}\n")
-    # Get the Battery manufacturer namedd
+    print(f"  Battery manufacturer  :   {battery.manufacturer}\n")
+    # Get the Battery manufacturer name
 
-    print(f"Battery Type : {battery.type}\n")
+    print(f"  Battery Type          :   {battery.type}\n")
     # Get the battery type (Internal Battery or external)
 
-    print(f"battery Chemistry : {battery.chemistry}\n")
+    print(f"  Battery Chemistry     :   {battery.chemistry}\n")
     # Get the Battery chemistry technology (Lithium-Ion in most cases)
 
-    print(f"Battery Charge : {battery.battery_percentage}\n")
+    print(f"  Battery Charge        :   {battery.battery_percentage}%\n")
     # Get the battery percentage (charging level)
 
-    print(f"Battery Health : {battery.battery_health}\n")
+    print(f"  Battery Health        :   {battery.battery_health}%\n")
     # Get the battery health or battery maximum capacity in percentage
 
-    print(f"Is Fast Charge ? : {'YES' if battery.is_fast_charging else 'NO'}\n")
+    print(f"  Is Fast Charge ?      :   {'YES' if battery.is_fast_charging else 'NO'}\n")
     # tells if the battery is fast charging or not
 
-    print(f"Is Charging ? : {'YES' if battery.is_plugged else 'NO'}\n")
+    print(f"  Is Charging ?         :   {'YES' if battery.is_plugged else 'NO'}\n")
     # tells if the battery is plugged to the power source
 
-    print(f"Battery Voltage : {battery.get_current_voltage()}\n")
+    print(f"  Battery Voltage       :   {battery.get_current_voltage()}\n")
     # Get the current battery output voltage
 
-    print(f"Power Mode : {battery.power_management_mode}\n")
+    print(f"  Power Mode            :   {battery.power_management_mode(aliased=True)}\n")
     # Get the power management mode is it 'balanced' or 'performance' or 'economy'
-    input("PRESS ENTER TO QUIT .")
 
 
 if __name__ == "__main__":
-    main()
+
+    try:
+        # Store the start time
+        s_time: float = perf_counter()
+        main()
+
+        # print the execution duration
+        print(f"  [ Finished in : {perf_counter() - s_time:.3f} s ]\n")
+
+    except NotSupportedPlatform:
+        print("\n   BatteryPy do not support this OS\n")
+    except NotSupportedDeviceType:
+        # This will occur when BatteryPy running on PC
+        print("\n   BatteryPy do not support Desktop Computer\n")
+    except NotSupportedDriver:
+        print("\n   BatteryPy can not reach the needed driver to run.\n")
+
+    # remove the report html file
+    os.system("del battery-report.html")
+
+    input("  PRESS ENTER TO QUIT .")
