8d178f5a1b9e69726ba4952fac4be8b630676458
Mr Aymen <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2025-06-05 02:57:44 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,14 @@
+"""
+@author : Aymen Brahim Djelloul
+version : 1.3
+date : 04.04.2025
+license : MIT License
+
+"""
+
+# IMPORTS
+from batterypy.app import main
+
+
+if __name__ == "__main__":
+    main()
