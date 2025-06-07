115c572cb61eb77647ea59ac2c141cee9d798125
Mr Aymen <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2025-06-07 16:00:25 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
-
-import batterypy
-from time import perf_counter
-
-r = batterypy.Battery()
-
-s_time = perf_counter()
-print(r.manufacturer)
-
-print(f"Finished in : {perf_counter() - s_time:.4f}")
+#
+# import batterypy
+# from time import perf_counter
+#
+#
+# r = batterypy.Battery(dev_mode=True)
+#
+# s_time = perf_counter()
+# # print(r.manufacturer)
+# # print(r.battery_percent)
+# # print(r.battery_health)
+# # print(r.battery_technology)
+# # print(r.cycle_count)
+# # print(r.charge_rate())
+# # print(r.is_fast_charge())
+# # print(r.is_plugged())
+# # print(r.battery_voltage())
+# print(r.battery_temperature())
+#
+#
+# print(f"Finished in : {perf_counter() - s_time:.4f}")
