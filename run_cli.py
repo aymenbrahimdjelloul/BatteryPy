2a545cbb0f2169293479e2bc24e5d0484e6bcb9a
Mr Aymen <92776194+aymenbrahimdjelloul@users.noreply.github.com>
2025-06-21 00:18:05 +0100

    Add files via upload

--- run_cli.py
+++ run_cli.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python3
+
 """
-@author : Aymen Brahim Djelloul
-version : 1.4
-date : 08.06.2025
+This code or file is part of 'BatteryPy' project
+copyright (c) 2023-2025 , Aymen Brahim Djelloul, All rights reserved.
+use of this source code is governed by MIT License that can be found on the project folder.
+
+@_AUTHOR : Aymen Brahim Djelloul
+VERSION : 1.5
+date : 19.06.2025
 License : MIT License
 
 """
 
 # IMPORTS
 from batterypy.cli import __main__
-
 
 if __name__ == "__main__":
     __main__()
